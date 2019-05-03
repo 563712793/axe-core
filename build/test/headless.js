6ffb215afb441f84b3c70e8deaa92dcb0a6485f6
jkodu <jey.nandakumar@gmail.com>
2019-05-03 15:47:48 +0100

    try pup

--- build/test/headless.js
+++ build/test/headless.js
@@ -1,30 +1,42 @@
-const getTestFiles = require('./headless/get-test-files');
-const runner = require('./headless/runner');
+// const getTestFiles = require('./headless/get-test-files');
+// const runner = require('./headless/runner');
+const puppeteer = require('puppeteer');
 
 const init = async () => {
-	console.time(`Headless tests duration`);
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '-–disable-setuid-sandbox'],
+		ignoreHTTPSErrors: true
+	});
+	const page = await browser.newPage();
+	await page.goto('https://google.com');
+	await page.screenshot({ path: 'example.png' });
 
-	process.setMaxListeners(Infinity);
+	await browser.close();
 
-	const urls = await getTestFiles();
-	try {
-		const { result } = await Promise.all(
-			urls.map(async url => await runner({ url }))
-		);
-		const { stats } = result;
+	// console.time(`Headless tests duration`);
 
-		// Print failures, if any
-		if (stats.failures > 0) {
-			console.log(`Failures: ${stats.failures}\n`);
-			result.failures.forEach(test => {
-				console.log(JSON.stringify(test, undefined, 2));
-			});
-		}
-	} catch (error) {
-		console.error(error);
-	}
+	// process.setMaxListeners(Infinity);
 
-	console.timeEnd(`Headless tests duration`);
+	// const urls = await getTestFiles();
+	// try {
+	// 	const { result } = await Promise.all(
+	// 		urls.map(async url => await runner({ url }))
+	// 	);
+	// 	const { stats } = result;
+
+	// 	// Print failures, if any
+	// 	if (stats.failures > 0) {
+	// 		console.log(`Failures: ${stats.failures}\n`);
+	// 		result.failures.forEach(test => {
+	// 			console.log(JSON.stringify(test, undefined, 2));
+	// 		});
+	// 	}
+	// } catch (error) {
+	// 	console.error(error);
+	// }
+
+	// console.timeEnd(`Headless tests duration`);
 };
 
 init();
